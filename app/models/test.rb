f173ea61275aa9e49d7b814eb12133633e532bc1
Nikita <nekitgo80@gmail.com>
2023-01-16 20:38:58 +0300

    fix: replace category.name to test.title

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  def self.test_titles_by_category_name(category_name)
-    Test.joins(:category).where("categories.name LIKE ?", "%#{category_name}%").pluck(:name)
+  def self.titles_by_category_name(category_name)
+    Test.joins(:category).where("categories.name LIKE ?", "%#{category_name}%").order(title: :desc).pluck(:title)
   end
 end
