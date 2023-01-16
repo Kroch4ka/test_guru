82a3c3efc8c5a4c4a8675bf048b37c465ec87c56
Nikita <nekitgo80@gmail.com>
2023-01-16 11:05:30 +0300

    build: add references to create_table migrations

--- db/migrate/20230116074033_create_tests.rb
+++ db/migrate/20230116074033_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, default: 0
+      t.references :category, index: true, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20230116074755_create_answers.rb
+++ db/migrate/20230116074755_create_answers.rb
@@ -2,7 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.text :body, null: false
-      t.boolean :correct, default: false
+      t.references :user, index: true, foreign_key: true
+      t.references :question, index: true, foreign_key: true
       t.timestamps
     end
   end
