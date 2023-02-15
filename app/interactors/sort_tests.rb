92fb3403652de02d9bb333ea2e1fb5193c35c72e
Nikita <nekitgo80@gmail.com>
2023-02-15 22:54:02 +0300

    fix: add title desc order param

--- app/interactors/sort_tests.rb
+++ app/interactors/sort_tests.rb
@@ -5,6 +5,7 @@ class SortTests
     order = context.order || 'title_asc'
     case order
     when 'title_asc' then context.tests = Test.all.order(title: :asc)
+    when 'title_desc' then context.tests = Test.all.order(title: :desc)
     else context.tests = Test.all
     end
   end
