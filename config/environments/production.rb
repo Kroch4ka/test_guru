bff3c9136cc712b7823aceaea5f217966be9b4fb
Nikita <nekitgo80@gmail.com>
2023-02-20 20:02:08 +0300

    fix assets compile flag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.hosts << "0.0.0.0:#{ENV['PORT']}"
 
